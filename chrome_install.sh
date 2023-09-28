b812df8ee3d29a97558d6f0208fd0e6f0b2c654a
Bruno Machado Ferreira <bruno5289@hotmail.com>
2023-09-28 08:58:15 -0400

    Update chrome_install.sh

--- chrome_install.sh
+++ chrome_install.sh
@@ -4,6 +4,16 @@ echo "Instalando Google Chrome..."
 
 #Atualizando repositórios do sistema
 sudo apt update && sudo apt upgrade
+
+#Verificando erros
+update_status=$?
+if [ $update_status -ne 0 ]; then
+  echo Ocorreu um erro ao atualizar os pacotes.
+  echo Os processos foram mortos. Reinicie o instalador.
+  sudo pkill -9 -f "$(basename "$0")" &> /dev/null
+  sudo pkill -9 -f "apt" &> /dev/null
+  exit 1
+fi
  
 # Baixando o pacote .deb do Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -18,4 +28,5 @@ echo "Google Chrome instalado com sucesso!"
 
 # Limpando o pacote baixado
 rm google-chrome-stable_current_amd64.deb
+rm wget-log*
 
