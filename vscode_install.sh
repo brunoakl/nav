ce4175f90c5950b069813be2b83c807d44835f11
Bruno Machado Ferreira <bruno5289@hotmail.com>
2023-09-28 08:58:28 -0400

    Update vscode_install.sh

--- vscode_install.sh
+++ vscode_install.sh
@@ -19,6 +19,16 @@ mostrar_animacao() {
     done
 }
 
+#Verificando erros
+update_status=$?
+if [ $update_status -ne 0 ]; then
+  echo Ocorreu um erro ao atualizar os pacotes.
+  echo Os processos foram mortos. Reinicie o instalador.
+  sudo pkill -9 -f "$(basename "$0")" &> /dev/null
+  sudo pkill -9 -f "apt" &> /dev/null
+  exit 1
+fi
+
 # Atualiza os pacotes do sistema
 sudo apt-get update &> /dev/null && sudo apt-get upgrade &> /dev/null
 mostrar_animacao "Atualizando pacotes do sistema" 5
